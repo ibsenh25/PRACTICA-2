a8b20c34a6e123db7658eccadee9059348d836b9
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-03-27 09:03:37 -0500

    CORRECION PROBLEMA_9.cpp

--- PROBLEMA_9.cpp
+++ PROBLEMA_9.cpp
@@ -2,41 +2,66 @@
 #include "PROBLEMA_9.h"
 using namespace std;
 
-
-void separar_numeros(char *origen, char *texto, char *numeros) {
-    char *ptr_origen = origen;
-    char *ptr_texto = texto;
-    char *ptr_numeros = numeros;
-
-    while (*ptr_origen != '\0') {
-        if (*ptr_origen >= '0' && *ptr_origen <= '9') {
-            *ptr_numeros = *ptr_origen;
-            ptr_numeros++;
-        } else {
-            *ptr_texto = *ptr_origen;
-            ptr_texto++;
-        }
-        ptr_origen++;
+int obtener_longitud(const char *cadena) {
+    int len = 0;
+    while (*cadena != '\0') {
+        len++;
+        cadena++;
     }
-
-    *ptr_texto = '\0';
-    *ptr_numeros = '\0';
+    return len;
 }
 
+int sumar_cadena(int n, const char *cadena) {
+    int len = obtener_longitud(cadena);
+    int suma_total = 0;
+    int valor_actual = 0;
+    int multiplicador = 1;
+    int cifras_contadas = 0;
+
+    const char *ptr = cadena + (len - 1);
+
+    while (ptr >= cadena) {
+       
+        int digito = *ptr - '0';
+       
+        valor_actual = valor_actual + (digito * multiplicador);
+        multiplicador *= 10;
+        cifras_contadas++;
+
+        if (cifras_contadas == n) {
+            suma_total += valor_actual;
+           
+            valor_actual = 0;
+            multiplicador = 1;
+            cifras_contadas = 0;
+        }
+       
+        ptr--;
+    }
+
+ 
+    if (cifras_contadas > 0) {
+        suma_total += valor_actual;
+    }
+
+    return suma_total;
+}
 
 int imple_9() {
-    char cadena[100];
-    char texto[100];
-    char numeros[100];
-
-    cout << "Ingrese una cadena: ";
+    int n;
+    char cadena[100];// Arreglo estático clásico
+   
+    cout << "Ingrese la cadena: ";
     cin >> cadena;
+   
+    cout << "Ingrese un numero para dividir la cadena: ";
+    cin >> n;
+   
+    cout << "Original: " << cadena << "." << endl;
+    cout << "Suma: " << sumar_cadena(n, cadena) << "." << endl;
 
-    separar_numeros(cadena, texto, numeros);
-
-    cout << "Original: " << cadena << endl;
-    cout << "Texto: " << texto << endl;
-    cout << "Numeros: " << numeros << endl;
+    return 0;
+}    cout << "Numeros: " << numeros << endl;
 
     return 0;
 }
