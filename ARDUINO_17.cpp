4cdade6dc92675b1665be9bca7b7235a3b2dc612
ibsenh25 <ibsen.henriquez@udea.edu.co>
2026-03-27 11:11:28 -0500

    CORRECCION ARDUINO_17.cpp

--- ARDUINO_17.cpp
+++ ARDUINO_17.cpp
@@ -1,15 +1,16 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void suma_divisores(int n, int *resultado){
     *resultado = 0;
-    if (n <= 1){
-        return;
-    }
+    if (n <= 1) return;
 
     *resultado = 1;
 
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             *resultado += i;
-
             if (i * i != n) {
                 *resultado += (n / i);
             }
@@ -18,39 +19,60 @@ void suma_divisores(int n, int *resultado){
 }
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Ingrese el numero limite en el monitor serial:");
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  
+  Serial.println("Ingrese el limite en el monitor serial:");
+  
+  lcd.print("Ingrese limite");
+  lcd.setCursor(0, 1);
+  lcd.print("en el serial...");
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        int limite = Serial.parseInt();
+  if (Serial.available() > 0) {
+    int limite = Serial.parseInt();
+    
+    if (limite > 0) {
+      Serial.print("\nLimite ingresado: ");
+      Serial.println(limite);
+      
+      lcd.clear();
+      lcd.print("Calculando...");
 
-        if (limite > 0) {
-            Serial.print("Limite ingresado: ");
-            Serial.println(limite);
+      int suma_total = 0;
+      int *ap_suma = &suma_total;
 
-            int suma_total = 0;
-            int *ap_suma = &suma_total;
+      for (int a = 2; a < limite; a++) {
+          int b = 0;
+          suma_divisores(a, &b);
 
-            for (int a = 2; a < limite; a++) {
-                int b = 0;
-                suma_divisores(a, &b);
+          if (b != a) {
+              int suma_b = 0;
+              suma_divisores(b, &suma_b);
 
-                if (b != a) {
-                    int suma_b = 0;
-                    suma_divisores(b, &suma_b);
+              if (suma_b == a) {
+                  *ap_suma += a;
+              }
+          }
+      }
 
-                    if (suma_b == a) {
-                        *ap_suma += a;
-                    }
-                }
-            }
-
-            Serial.print("El resultado de la suma es: ");
-            Serial.print(*ap_suma);
-            Serial.println(".");
-            Serial.println("\nIngrese otro limite para volver a calcular:");
-        }
+      Serial.print("El resultado de la suma es: ");
+      Serial.print(*ap_suma);
+      Serial.println(".");
+      
+      lcd.clear();
+      lcd.print("Suma amigables:");
+      lcd.setCursor(0, 1); 
+      lcd.print(*ap_suma);
+      
+      delay(5000); 
+      
+      lcd.clear();
+      lcd.print("Ingrese limite");
+      lcd.setCursor(0, 1);
+      lcd.print("en el serial:");
+      Serial.println("\nIngrese otro limite para volver a calcular:");
     }
+  }
 }
